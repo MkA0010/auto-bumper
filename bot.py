5daf26afea887f4dd1b55421bc634b23a7b9344c
MkA0010 <mikolajapelski92@gmail.com>
2026-07-16 12:58:44 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 import discord
 from discord.ext import tasks
 
-TOKEN = "MTUyNzI0Mzc5MzAxMzkzMjAzNw.Gly200.I_NNYfxduazLfGiggEXrvVnqc9ymoEY6GER3TM"
+TOKEN = os.getenv("TOKENMTUyNzI0Mzc5MzAxMzkzMjAzNw.G05KnI.ghqTTPp9W3JuvIbLCVkQsi8PlacRyijepf0orw"MTUyNzI2MzU4NTgzMzU4Njg0Mg.GV3qRg.pVrJ0ahf1EeS4AJPQo-6-n9JLi7U_QA2IefMto)
 
 CHANNEL_ID = 1490472870353174528
 
@@ -15,10 +15,10 @@ async def on_ready():
     print("Bot działa!")
     send_message.start()
 
-@tasks.loop(minutes=10)
+@tasks.loop(minutes=120)
 async def send_message():
     channel = client.get_channel(CHANNEL_ID)
     if channel:
-        await channel.send("!daily")
+        await channel.send("/bump")
 
 client.run(TOKEN)
